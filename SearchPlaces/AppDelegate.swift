5cb0636864f863b820e9eea01721b3145452cdf8
Olya <olyat96@gmail.com>
2018-02-27 20:39:08 +0300

    App is requesting the user location and show places

--- SearchPlaces/AppDelegate.swift
+++ SearchPlaces/AppDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import GooglePlaces
+import GoogleMaps
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,7 +17,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        GMSPlacesClient.provideAPIKey("AIzaSyArYlESrpqIn5b-3JbcRdGoLi41Cn6yJwY")
+        GMSServices.provideAPIKey("AIzaSyArYlESrpqIn5b-3JbcRdGoLi41Cn6yJwY")
+        
         return true
     }
 
--- SearchPlaces/ViewController.swift
+++ SearchPlaces/ViewController.swift
@@ -7,19 +7,53 @@
 //
 
 import UIKit
+import GooglePlaces
+import GooglePlacePicker
+import GoogleMaps
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    @IBOutlet var mapViewContainer: UIView!
+    
+    var googleMapView: GMSMapView!
+    var locationManager: CLLocationManager!
+    var placePicker: GMSPlacePicker!
+    var latitude: Double!
+    var longitude: Double!
+    
+    @IBAction func showSearchController(sender: AnyObject) {
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.locationManager = CLLocationManager()
+        self.locationManager.delegate = self
+        self.locationManager.requestAlwaysAuthorization()
+        self.locationManager.startUpdatingLocation()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        locationManager.requestWhenInUseAuthorization()
+        self.googleMapView = GMSMapView(frame: self.mapViewContainer.frame)
+        self.googleMapView.animate(toZoom: 18.0)
+        self.view.addSubview(googleMapView)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func locationManager(_ manager: CLLocationManager,
+                         didUpdateLocations locations: [CLLocation]){
+        
+        let location:CLLocation = locations.last!
+        self.latitude = location.coordinate.latitude
+        self.longitude = location.coordinate.longitude
+        
+        let coordinates = CLLocationCoordinate2DMake(self.latitude, self.longitude)
+        let marker = GMSMarker(position: coordinates)
+        marker.title = "I am here"
+        marker.map = self.googleMapView
+        self.googleMapView.animate(toLocation: coordinates)
     }
-
-
+   
 }
 
